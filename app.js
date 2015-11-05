8d9bb4d41aef1c3f70f43194d24b69c9e321d995
oravaantti <orava.antti@gmail.com>
2015-11-05 08:51:06 +0200

    app.js modified

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
 var database = require("./app_modules/database");
 var friends_rest = require("./app_modules/friends_rest");
@@ -20,6 +21,8 @@ app.use(function(req, res, next){
 app.use(express.static(path.join(__dirname, "views")));
 app.use("/lib", express.static(path.join(__dirname, "lib")));
 app.use("/scr", express.static(path.join(__dirname, "scr")));
+
+app.use(bodyParser());
 
 app.use("/friend", friends_rest);
 
